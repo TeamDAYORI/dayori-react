2b5d9dbc331fde507ff3d4e98bdfbf0a0d807965
jhkang9820 <jhkang9820@gmail.com>
2023-07-07 17:34:24 +0900

    feat: [Setting] router 연결

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Main from "./pages/Main";
+import User from "features/user/User";
+import Diary from "features/diary/Diary";
+import Page from "features/page/Page";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/page" element={<Page />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
