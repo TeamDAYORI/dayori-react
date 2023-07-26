5b3b5613cd120bc0f3843153e88cc98de407971f
jhkang9820 <jhkang9820@gmail.com>
2023-07-27 00:01:23 +0900

    feat: [Common] 기본 페이지 생성 - kjh

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import Main from "./pages/Main";
 import User from "features/user/User";
 import Diary from "features/diary/Diary";
 import Page from "features/page/Page";
+import Header from "components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/diary" element={<Diary />} />
-        <Route path="/page" element={<Page />} />
-      </Routes>
+      <div className="App-container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/page" element={<Page />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,27 @@
-export {};
+import React from "react";
+import { styled } from "styled-components";
+
+const StyledTitleBar = styled.div`
+  min-height: 3vh;
+  padding: 0 10px 0 10px;
+  background: #ff6e7f; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #bfe9ff, #ff6e7f); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #bfe9ff,
+    #ff6e7f
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+
+const Header = () => {
+  return (
+    <StyledTitleBar className="title-bar">
+      <div className="title-bar-text">DAYORI</div>
+      <div className="title-bar-controls">
+        <button aria-label="Close" />
+      </div>
+    </StyledTitleBar>
+  );
+};
+
+export default Header;
