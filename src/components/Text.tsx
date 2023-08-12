bf443ec9678da50d59cf99c0b45d3e42d06ee45c
jhkang9820 <jhkang9820@gmail.com>
2023-08-12 16:38:26 +0900

    feat: [Common] 폰트 컴포넌트 생성 - kjh

---
+++ src/components/Text.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "components/css/Text.module.css";
+
+interface Props {
+  value: string;
+  type?: "caption" | "text" | "title";
+  bold?: boolean;
+}
+
+const Text = ({ value, type, bold }: Props) => {
+  const style = {
+    fontWeight: bold ? "600" : "400",
+  };
+  return (
+    <>
+      <span className={`${styles[type]}`} style={style}>
+        {value}
+      </span>
+    </>
+  );
+};
+
+Text.defaultProps = {
+  value: "내용이 들어갑니다.",
+  type: "sample",
+};
+
+export default Text;
