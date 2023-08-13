3406939af385d1dfbbe21002ed077179c10a9e74
jhkang9820 <jhkang9820@gmail.com>
2023-08-13 23:25:42 +0900

    feat: [Common] store persist 적용 - kjh

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import authReducer from "slices/authSlice";
+import pageReducer from "slices/pageSlice";
 
 const rootReducer = combineReducers({
-  // 설정한 reducer
+  auth: authReducer,
+  page: pageReducer,
 });
 
-const store = configureStore({
-  reducer: rootReducer,
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["auth"],
+  timeout: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
 
-export default store;
+export const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
 export type rootState = ReturnType<typeof store.getState>;
