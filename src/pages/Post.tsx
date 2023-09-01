a4d00edb0309fc7199bc674e6c6e6a96302d9290
jhkang9820 <jhkang9820@gmail.com>
2023-09-01 19:28:28 +0900

    [FE] feat: 페이지 작성 컴포넌트 생성

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "../features/page/Page.module.css";
+import PageWrite from "features/page/PageWrite";
+import PageWriteInfo from "features/page/PageWriteInfo";
 
 const Post = () => {
-  return <div>POST</div>;
+  return (
+    <div className={styles.post_container}>
+      <PageWrite />
+      <PageWriteInfo />
+    </div>
+  );
 };
 
 export default Post;
